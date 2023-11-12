4a1bfd0825d973e6b2cca1a25839c2419518d646
kazburrel <oegbosionu@gmail.com>
2023-11-12 15:21:55 +0100

    progress

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,19 +6,8 @@ import "./bootstrap";
 
 // Alpine.start();
 import { createApp } from "vue";
-import { createRouter, createWebHistory } from "vue-router";
 import App from "./layouts/App.vue";
-import PostsIndex from "./components/Posts/Index.vue";
-import PostsCreate from "./components/Posts/Create.vue";
+import router from './routes/index' 
 
-const routes = [
-    { path: "/", component: PostsIndex },
-    { path: "/posts/create", component: PostsCreate },
-];
-
-const router = createRouter({
-    history: createWebHistory(),
-    routes,
-});
 
 createApp(App).use(router).mount("#app");
