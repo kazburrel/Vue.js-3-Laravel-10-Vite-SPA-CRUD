f5d8c0a92ff09f38d0956b44de88ad65eba2315c
kazburrel <oegbosionu@gmail.com>
2023-11-20 12:52:58 +0100

    final

--- resources/js/composables/auth.js
+++ resources/js/composables/auth.js
@@ -1,6 +1,8 @@
 // import axios from "axios";
 import { ref, reactive, inject } from "vue";
 import { useRouter } from "vue-router";
+import { AbilityBuilder, Ability } from '@casl/ability'; 
+import { ABILITY_TOKEN } from '@casl/vue'; 
 const user = reactive({
     name: "",
     email: "",
@@ -11,6 +13,7 @@ export default function useAuth() {
     const validationErrors = ref({});
     const router = useRouter();
     const swal = inject("$swal");
+    const ability = inject(ABILITY_TOKEN) 
     const loginForm = reactive({
         email: "",
         password: "",
@@ -58,12 +61,13 @@ export default function useAuth() {
             .finally(() => (processing.value = false));
     };
 
-    const loginUser = (response) => {
+    const loginUser = async (response) => { 
         user.name = response.data.name;
         user.email = response.data.email;
         // console.log(user.name);
         localStorage.setItem("loggedIn", JSON.stringify(true));
-        router.push({ name: "posts.index" });
+        await getAbilities() 
+        await router.push({ name: 'posts.index' }) 
     };
 
     const getUser = () => {
@@ -93,6 +97,18 @@ export default function useAuth() {
             });
     };
 
+    const getAbilities = async() => { 
+        axios.get('/api/abilities')
+            .then(response => {
+                const permissions = response.data
+                const { can, rules } = new AbilityBuilder(Ability)
+ 
+                can(permissions)
+ 
+                ability.update(rules)
+            })
+    } 
+
     return {
         loginForm,
         validationErrors,
@@ -103,5 +119,6 @@ export default function useAuth() {
         user,
         getUser,
         logout,
+        getAbilities
     };
 }
--- routes/api.php
+++ routes/api.php
@@ -23,13 +23,21 @@ use Illuminate\Support\Facades\Route;
 // Route::apiResource('posts', PostController::class); 
 // Route::get('categories', [CategoryController::class, 'index']); 
 // Route::put('post/{post}', [PostController::class, 'update']); 
-Route::group(['middleware' => 'auth:sanctum'], function() { 
+Route::group(['middleware' => 'auth:sanctum'], function () {
     Route::apiResource('posts', PostController::class);
     Route::get('categories', [CategoryController::class, 'index']);
-    Route::get('/user', function (Request $request) { 
+    Route::get('/user', function (Request $request) {
         // dd($request->user()->name);
         return $request->user();
     });
+    Route::get('abilities', function (Request $request) {
+        return $request->user()->roles()->with('permissions')
+            ->get()
+            ->pluck('permissions')
+            ->flatten()
+            ->pluck('name')
+            ->unique()
+            ->values()
+            ->toArray();
+    });
 });
-
-
